f09fa2dfe9cba7cee7d49b530f66e6596b946cd9
marcuslchoi <marcuslchoi@gmail.com>
2022-01-24 13:39:41 -0800

    Initial Commit

---
+++ Push-To-Talk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Push-To-Talk
+//
+//  Created by Marcus Choi on 1/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
