2bf169305faa828780558055dfd1dc6434a42bfc
marcuslchoi <marcuslchoi@gmail.com>
2022-01-24 14:44:41 -0800

    add finishRecording(success), config record button based on its state

--- Push-To-Talk/ViewController.swift
+++ Push-To-Talk/ViewController.swift
@@ -12,17 +12,25 @@ class ViewController: UIViewController {
 
     var recordingSession: AVAudioSession!
     var audioRecorder: AVAudioRecorder!
+    var isRecording: Bool { return audioRecorder != nil }
     @IBOutlet weak var btnRecord: UIButton!
+    let recordingImage = UIImage(systemName: SFSymbols.mic)
+    let stopImage = UIImage(systemName: SFSymbols.stop)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         requestRecordingPermission()
-        addRecordButton()
+        configureRecordButton()
     }
     
-    private func addRecordButton() {
-        let recordingImage = UIImage(systemName: SFSymbols.mic)
-        btnRecord.setImage(recordingImage, for: .normal)
+    private func configureRecordButton() {
+        if isRecording {
+            btnRecord.setImage(stopImage, for: .normal)
+            btnRecord.setTitle("Stop", for: .normal)
+        } else {
+            btnRecord.setImage(recordingImage, for: .normal)
+            btnRecord.setTitle("Record", for: .normal)
+        }
     }
 
     private func requestRecordingPermission() {
@@ -47,19 +55,36 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onButtonRecordPress(_ sender: Any) {
-        startRecording()
+        if isRecording {
+            finishRecording(success: true)
+        }
+        else {
+            startRecording()
+        }
+        configureRecordButton()
     }
     
     func startRecording() {
         let audioFilename = K.localDocsUrl.appendingPathComponent("recording.m4a")
-
         do {
             audioRecorder = try AVAudioRecorder(url: audioFilename, settings: K.recordingSettings)
             audioRecorder.delegate = self
             audioRecorder.record()
-            btnRecord.setTitle("Tap to Stop", for: .normal)
         } catch {
-            //finishRecording(success: false)
+            finishRecording(success: false)
+        }
+    }
+    
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+
+        DispatchQueue.main.async {
+            if success {
+                self.showOkAlert(title: "Success!", msg: "Recording succeeded.")
+            } else {
+                self.showOkAlert(title: "Fail!", msg: "Recording failed.")
+            }
         }
     }
 }
