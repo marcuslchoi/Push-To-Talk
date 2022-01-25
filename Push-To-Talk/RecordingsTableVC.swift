8bb770266eccac8bf0635c611fd3c060a021250b
marcuslchoi <marcuslchoi@gmail.com>
2022-01-24 16:30:07 -0800

    play audio on pressing play button on recording cell

--- Push-To-Talk/RecordingsTableVC.swift
+++ Push-To-Talk/RecordingsTableVC.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordingsTableVC: UITableViewController {
 
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Recordings"
@@ -38,6 +41,44 @@ class RecordingsTableVC: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RecordingCell", for: indexPath) as! RecordingCell
         guard let localFilenames = RecordingManager.shared.getLocalFileNames() else { return cell }
         cell.lblName.text = localFilenames[indexPath.row]
+        
+        cell.btnPlay.tag = indexPath.row
+        cell.btnPlay.addTarget(self, action: #selector(onButtonPlayPress), for: .touchUpInside)
+        cell.btnDelete.tag = indexPath.row
+        cell.btnDelete.addTarget(self, action: #selector(onButtonDeletePress), for: .touchUpInside)
+        
         return cell
+    }
+    
+    @objc func onButtonPlayPress(sender: UIButton) {
+        let row = sender.tag
+        playAudio(at: row)
+    }
+    
+    func playAudio(at row: Int) {
+        //todo show error notification
+        guard let localUrls = RecordingManager.shared.getLocalDocURLs(), localUrls.count > row else { return }
+        let audioFilename = localUrls[row]
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFilename)
+            audioPlayer.delegate = self
+            audioPlayer.play()
+        } catch {
+            showOkAlert(title: "Error", msg: "There was a problem playing the audio file.")
+        }
+    }
+    
+    @objc func onButtonDeletePress(sender: UIButton) {
+        let row = sender.tag
+    }
+}
+
+extension RecordingsTableVC: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print("Audio player finished playing")
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        print("Audio Play Decode Error")
     }
 }
