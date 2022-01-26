45f09986facc209db1608e23739b4199e17bfafe
marcuslchoi <marcuslchoi@gmail.com>
2022-01-25 16:35:35 -0800

    move name recording alert to RecordVC
    This was in UIViewcontroller extensions, but it only has to do with the RecordVC, not all viewcontrollers

--- Push-To-Talk/Extensions/UIViewController+Ext.swift
+++ Push-To-Talk/Extensions/UIViewController+Ext.swift
@@ -17,27 +17,4 @@ extension UIViewController {
             self.present(alert, animated: true, completion: nil)
         }
     }
-    
-    func showAlertWithTextField(title: String, msg: String)
-    {
-        DispatchQueue.main.async {
-            let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
-                if let newName = alert.textFields?.first?.text {
-                    if !newName.isEmpty {
-                        RecordingManager.shared.renameFile(newName: newName)
-                    }
-                    else {
-                        let dateStr = RecordingManager.shared.getCurrentDateString()
-                        RecordingManager.shared.renameFile(newName: dateStr)
-                    }
-                }
-            }
-            alert.addTextField { textField in
-                textField.placeholder = "input recording name..."
-            }
-            alert.addAction(okAction)
-            self.present(alert, animated: true, completion: nil)
-        }
-    }
 }
--- Push-To-Talk/RecordVC.swift
+++ Push-To-Talk/RecordVC.swift
@@ -82,9 +82,32 @@ class RecordVC: UIViewController {
         audioRecorder = nil
 
         if success {
-            self.showAlertWithTextField(title: "Success!", msg: "Please name your recording.")
+            self.showNameRecordingAlert(title: "Success!", msg: "Please name your recording.")
         } else {
             self.showOkAlert(title: "Fail!", msg: "Recording failed.")
+        }
+    }
+    
+    func showNameRecordingAlert(title: String, msg: String)
+    {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                if let newName = alert.textFields?.first?.text {
+                    if !newName.isEmpty {
+                        RecordingManager.shared.renameFile(newName: newName)
+                    }
+                    else {
+                        let dateStr = RecordingManager.shared.getCurrentDateString()
+                        RecordingManager.shared.renameFile(newName: dateStr)
+                    }
+                }
+            }
+            alert.addTextField { textField in
+                textField.placeholder = "input recording name..."
+            }
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
         }
     }
 }
